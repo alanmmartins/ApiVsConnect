e7092e9c3d034baed83da58be7fde5a2f76d499a
Alan Munoz Martins <alanmunozmartins@gmail.com>
2023-11-07 21:51:28 -0300

    CRIAR TOKEN

--- src/main/java/com/senai/apivsconnect/config/SecurityConfig.java
+++ src/main/java/com/senai/apivsconnect/config/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.senai.apivsconnect.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,7 +17,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll()
+                ).build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new  BCryptPasswordEncoder ();
+    }
 }
---
+++ src/main/java/com/senai/apivsconnect/services/TokenService.java
@@ -0,0 +1,40 @@
+package com.senai.apivsconnect.services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.senai.apivsconnect.models.UsuarioModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+    private final Algorithm algoritimo = Algorithm.HMAC256(secret);
+
+    public String gerarToken(UsuarioModel usuario){
+        try{
+            String token = JWT.create()
+                    .withIssuer("api-vsconnect")
+                    .withSubject(usuario.getEmail())
+                    .withExpiresAt(gerarVlidadeToken())
+                    .sign(algoritimo);
+
+            return token;
+
+        }catch (JWTCreationException exception){
+            throw new RuntimeException("Error creating",exception);
+
+        }
+    }
+    private Instant gerarVlidadeToken(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+
+}
