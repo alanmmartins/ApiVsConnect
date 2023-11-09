d745192cdf9a29bfcc11860db919b650c5efdc19
Alan Munoz Martins <alanmunozmartins@gmail.com>
2023-11-08 21:56:07 -0300

    swagger

--- src/main/java/com/senai/apivsconnect/services/TokenService.java
+++ src/main/java/com/senai/apivsconnect/services/TokenService.java
@@ -15,23 +15,43 @@ import java.time.ZoneOffset;
 public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
-    private final Algorithm algoritimo = Algorithm.HMAC256(secret);
+
 
     public String gerarToken(UsuarioModel usuario){
         try{
+            // Cria um objeto Algorithm com o algoritmo HMAC256 e a chave secreta 'secret'
+            Algorithm algoritimo = Algorithm.HMAC256(secret);
+
+            // Cria um objeto JWT e começa a construção do token
             String token = JWT.create()
-                    .withIssuer("api-vsconnect")
-                    .withSubject(usuario.getEmail())
-                    .withExpiresAt(gerarVlidadeToken())
-                    .sign(algoritimo);
+
+                    .withIssuer("api-vsconnect")// Define o emissor como 'api-vsconnect'
+                    .withSubject(usuario.getEmail())// Define o assunto como o email do usuário
+                    .withExpiresAt(gerarVlidadeToken()) // Define a data de expiração do token
+                    .sign(algoritimo);// Assina o token com o algoritmo e a chave secreta
 
             return token;
 
         }catch (JWTCreationException exception){
+            // Em caso de erro ao criar o token, lança uma exceção com a mensagem de erro
             throw new RuntimeException("Error creating",exception);
 
         }
     }
+    public String validarToken(String token){
+        try {
+            Algorithm algoritimo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritimo)
+                    .withIssuer("api-vsconnect")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        }catch (JWTCreationException exception){
+            throw new RuntimeException(exception);
+        }
+    }
+
     private Instant gerarVlidadeToken(){
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
